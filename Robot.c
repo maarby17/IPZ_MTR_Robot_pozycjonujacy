efb5093dd1055d3839054acd5e720d63bc489dc0
przemyslawwojcik <80771893+przemyslawwojcik@users.noreply.github.com>
2021-05-16 19:48:45 +0200

    Update Robot.c

--- Robot.c
+++ Robot.c
@@ -1,24 +1,30 @@
-// First robot software on arduino for tests 
+// First version of robot software (only data)
 
-#include <RC5.h>
+#include <Wire.h>
+#include <LSM303.h>
 
-#define LED 13
-#define BUZZER 10
-#define PUSHBUTTON 2
-#define L_MOTOR_DIR 4
-#define L_MOTOR_PWM 5
-#define R_MOTOR_DIR 9
-#define R_MOTOR_PWM 6
-#define MOTOR_PWM_MAX 150
-#define TSOP_PIN 3
-#define SR04_FRONT_ECHO 7
-#define SR04_FRONT_TRIG 8
+#define LED                 13
+#define BUZZER              10
+#define PUSHBUTTON           2
+#define L_MOTOR_DIR          4
+#define L_MOTOR_PWM          5
+#define R_MOTOR_DIR          9
+#define R_MOTOR_PWM          6
+#define MOTOR_PWM_MAX      150
+#define TSOP_PIN             3
+#define SR04_FRONT_ECHO      7
+#define SR04_FRONT_TRIG      8
+#define SR04_BACK_ECHO      12
+#define SR04_BACK_TRIG      13
+#define SR04_RIGHT_ECHO     16
+#define SR04_RIGHT_TRIG     17
+#define SR04_LEFT_ECHO      14
+#define SR04_LEFT_TRIG      15
+#define LSM303D_ADDR      0x3a
 
-RC5 rc5(TSOP_PIN);
+LSM303 compass;
 
-byte address;
-byte command;
-byte toggle;
+char report[80];
 
  void setup() {
   // Setting up outputs
@@ -48,6 +54,19 @@ byte toggle;
   //Setting up echo sensors
   pinMode(SR04_FRONT_ECHO, INPUT);
   pinMode(SR04_FRONT_TRIG, OUTPUT);
+  pinMode(SR04_BACK_ECHO, INPUT);
+  pinMode(SR04_BACK_TRIG, OUTPUT);
+  pinMode(SR04_RIGHT_ECHO, INPUT);
+  pinMode(SR04_RIGHT_TRIG, OUTPUT);
+  pinMode(SR04_LEFT_ECHO, INPUT);
+  pinMode(SR04_LEFT_TRIG, OUTPUT);
+
+  // Setting up I2C
+  Wire.begin(); 
+
+  //Setting up compass
+  compass.init();
+  compass.enableDefault();
 }
 
  // Function for setting up speed and direction of left motor
@@ -112,38 +131,63 @@ void backward (void){
   stopMotors();
 }
 
-int distance(void){
+int distance(int _iPin_TRIG,int _iPin_ECHO){
   long _lTime, _lDistance;
 
-  digitalWrite(SR04_FRONT_TRIG, LOW);
+  digitalWrite(_iPin_TRIG, LOW);
   delayMicroseconds(2);
-  digitalWrite(SR04_FRONT_TRIG, HIGH);
+  digitalWrite(_iPin_TRIG, HIGH);
   delayMicroseconds(10);
-  digitalWrite(SR04_FRONT_TRIG, LOW);
+  digitalWrite(_iPin_TRIG, LOW);
 
-  _lTime = pulseIn(SR04_FRONT_ECHO, HIGH);
+  _lTime = pulseIn(_iPin_ECHO, HIGH);
   _lDistance = _lTime / 58;
 
   return _lDistance;
 }
 
+void compass_ping(void){
+  compass.read();
+  float _a_x = compass.a.x * 2.0f / 32678.0f;
+  float _a_y = compass.a.y * 2.0f / 32678.0f;
+  float _a_z = compass.a.z * 2.0f / 32678.0f;
+  
+  //snprintf(report, sizeof(report), "A: %f %f %f   M: %6d %6d %6d", _a_x, _a_y, _a_z, compass.m.x, compass.m.y, compass.m.z);
+  Serial.print("A: ");
+  Serial.print(_a_x); Serial.print(" ");
+  Serial.print(_a_y); Serial.print(" ");
+  Serial.println(_a_z);
+  Serial.print("M: ");
+  Serial.print(compass.m.x); Serial.print(" ");
+  Serial.print(compass.m.y); Serial.print(" ");
+  Serial.println(compass.m.z); 
+}
+
+
 void loop() {
-  Serial.print(distance());
-  Serial.println(" cm");
-   if(rc5.read(&toggle, &address, &command)){
-      switch(command){
-        case 2:
-          forward();
-        break;
-        case 4:
-          turnLeft();
-        break;
-        case 6:
-          turnRight();
-        break;
-        case 8:
-          backward();
-        break;
-      }
-   }
+  Serial.print(distance(SR04_FRONT_TRIG, SR04_FRONT_ECHO));
+  Serial.println(" cm front");
+  Serial.print(distance(SR04_BACK_TRIG, SR04_BACK_ECHO));
+  Serial.println(" cm back");
+  Serial.print(distance(SR04_RIGHT_TRIG, SR04_RIGHT_ECHO));
+  Serial.println(" cm right");
+  Serial.print(distance(SR04_LEFT_TRIG, SR04_LEFT_ECHO));
+  Serial.println(" cm left");
+
+  compass_ping();
+  //Serial.println(report); 
+  
+ 
+  // forward();
+  // delay(500);
+    
+  // turnLeft();
+  // delay(500);
+
+  // turnRight();
+  // delay(500);
+    
+  // backward();
+  delay(5000);
+   
 }
