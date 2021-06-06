f150e01720b5eaf3316caf6c30c4295c7ea90be4
przemyslawwojcik <80771893+przemyslawwojcik@users.noreply.github.com>
2021-06-06 22:39:50 +0200

    Update Robot.c

--- Robot.c
+++ Robot.c
@@ -21,10 +21,9 @@
 #define SR04_LEFT_ECHO      14
 #define SR04_LEFT_TRIG      15
 #define LSM303D_ADDR      0x3a
+#define ANGLE_NORTH        115
 
 LSM303 compass;
-
-char report[80];
 
  void setup() {
   // Setting up outputs
@@ -51,7 +50,7 @@ char report[80];
   digitalWrite(LED, LOW);
   delay(500);
 
-  //Setting up echo sensors
+  // Setting up echo sensors
   pinMode(SR04_FRONT_ECHO, INPUT);
   pinMode(SR04_FRONT_TRIG, OUTPUT);
   pinMode(SR04_BACK_ECHO, INPUT);
@@ -64,13 +63,19 @@ char report[80];
   // Setting up I2C
   Wire.begin(); 
 
-  //Setting up compass
+  // Setting up compass
   compass.init();
   compass.enableDefault();
+
+  // Compass calibration
+  compass.m_min = (LSM303::vector<int16_t>){-2568, -2729, -2019}; // -2568, -2729, -2019
+  compass.m_max = (LSM303::vector<int16_t>){+2887, +2629, +3849}; // +2887, +2629, +3849
+
+  // Setting up Z asix as a reference for compass
+  compass.heading((LSM303::vector<int>){0, 0, 1});
 }
 
- // Function for setting up speed and direction of left motor
-void leftMotorMove(int _iL){
+void leftMotorMove(int _iL){ // (F) Setting up speed and direction of left motor
   if (_iL >0) digitalWrite(L_MOTOR_DIR, 0);
   else {
     digitalWrite(L_MOTOR_DIR, 1);
@@ -79,10 +84,10 @@ void leftMotorMove(int _iL){
   
   _iL = map(_iL, 0, 100, 0, MOTOR_PWM_MAX);
   analogWrite(L_MOTOR_PWM, _iL);
+  Serial.println(_iL);
 }
 
-// Function for setting up speed and direction of right motor
-void rightMotorMove(int _iR){
+void rightMotorMove(int _iR){ // (F)  Setting up speed and direction of right motor
   if (_iR >0) digitalWrite(R_MOTOR_DIR, 0);
   else { 
     digitalWrite(R_MOTOR_DIR, 1);
@@ -91,47 +96,45 @@ void rightMotorMove(int _iR){
  
   _iR = map(_iR, 0, 100, 0, MOTOR_PWM_MAX);
   analogWrite(R_MOTOR_PWM, _iR);
+  Serial.print(" Right ");
+  Serial.println(_iR);
 }
 
-// Function for motors stop
-void stopMotors(void){
+void stopMotors(void){ // (F) Motors stop
    digitalWrite(L_MOTOR_DIR, 0);
    analogWrite(L_MOTOR_PWM, 0);
    digitalWrite(R_MOTOR_DIR, 0);
    analogWrite(R_MOTOR_PWM, 0);
 }
 
-// Function turning robot left by 90 degrees
-void turnLeft (void){
-  rightMotorMove(55); // 50 speed is minimum
-  delay(550);
+void turnLeft (int _iSpeed, int _iTime = 200){ // (F) Turn robot left with speed (50 - 100)
+  rightMotorMove(_iSpeed);
+  delay(_iTime);
   stopMotors();
 }
 
-// Function turning robot right by 90 degrees
-void turnRight (void){
-  leftMotorMove(55); // 50 speed is minimum
-  delay(550);
+void turnRight (int _iSpeed, int _iTime = 200){ // (F) Turns robot right with speed (50 - 100)
+  leftMotorMove(_iSpeed); // 50 speed is minimum
+  delay(_iTime);
   stopMotors();
 }
 
-// Function moving robot forward
-void forward (void){
-  leftMotorMove(50); // 50 speed is minimum
-  rightMotorMove(50);
-  delay(200);
+
+void forward (int _iSpeed, int _iTime = 200){ // (F) Move robot forward
+  leftMotorMove(_iSpeed); // 50 speed is minimum
+  rightMotorMove(_iSpeed);
+  delay(_iTime);
   stopMotors();
 }
 
-// Function moving robot Backward
-void backward (void){
-  leftMotorMove(-50); // 50 speed is minimum
-  rightMotorMove(-50);
-  delay(200);
+void backward (int _iSpeed, int _iTime = 200){ // (F) Move robot backward
+  leftMotorMove(-_iSpeed); // 50 speed is minimum
+  rightMotorMove(-_iSpeed);
+  delay(_iTime);
   stopMotors();
 }
 
-int distance(int _iPin_TRIG,int _iPin_ECHO){
+int getDistance(int _iPin_TRIG,int _iPin_ECHO){ // (F) Get distance from echo sensor
   long _lTime, _lDistance;
 
   digitalWrite(_iPin_TRIG, LOW);
@@ -146,48 +149,62 @@ int distance(int _iPin_TRIG,int _iPin_ECHO){
   return _lDistance;
 }
 
-void compass_ping(void){
+void compassPing(void){
   compass.read();
   float _a_x = compass.a.x * 2.0f / 32678.0f;
   float _a_y = compass.a.y * 2.0f / 32678.0f;
   float _a_z = compass.a.z * 2.0f / 32678.0f;
+
+  float heading = compass.heading();
   
-  //snprintf(report, sizeof(report), "A: %f %f %f   M: %6d %6d %6d", _a_x, _a_y, _a_z, compass.m.x, compass.m.y, compass.m.z);
   Serial.print("A: ");
   Serial.print(_a_x); Serial.print(" ");
   Serial.print(_a_y); Serial.print(" ");
   Serial.println(_a_z);
-  Serial.print("M: ");
-  Serial.print(compass.m.x); Serial.print(" ");
-  Serial.print(compass.m.y); Serial.print(" ");
-  Serial.println(compass.m.z); 
+  Serial.println(heading);
+}
+
+void headNorth(void){
+  int _iS;
+  
+  compass.read();
+  float heading = compass.heading();
+
+  while(heading > (ANGLE_NORTH + 10) || heading < (ANGLE_NORTH - 10)){
+    if(heading > (ANGLE_NORTH + 10)){
+      _iS = map(heading, (ANGLE_NORTH + 10), 360, 65, 100);
+      Serial.print(" Left ");
+      turnLeft(_iS, 25);
+    }
+    else{
+      if(heading < (ANGLE_NORTH - 10)){
+        _iS = map(heading, 0, (ANGLE_NORTH - 10), 100, 65);
+        turnRight(_iS, 25);
+      }
+    }
+  compass.read();
+  heading = compass.heading();
+  }
 }
 
 
 void loop() {
-  Serial.print(distance(SR04_FRONT_TRIG, SR04_FRONT_ECHO));
-  Serial.println(" cm front");
-  Serial.print(distance(SR04_BACK_TRIG, SR04_BACK_ECHO));
-  Serial.println(" cm back");
-  Serial.print(distance(SR04_RIGHT_TRIG, SR04_RIGHT_ECHO));
-  Serial.println(" cm right");
-  Serial.print(distance(SR04_LEFT_TRIG, SR04_LEFT_ECHO));
-  Serial.println(" cm left");
-
-  compass_ping();
-  //Serial.println(report); 
+  int _iTargetF, _iTargetB, _iTargetL, _iTargetR;
   
- 
-  // forward();
-  // delay(500);
-    
-  // turnLeft();
-  // delay(500);
+  
+  Serial.print(getDistance(SR04_FRONT_TRIG, SR04_FRONT_ECHO));
+  Serial.println(" cm front");
+  Serial.print(getDistance(SR04_BACK_TRIG, SR04_BACK_ECHO));
+  Serial.println(" cm back");
+  Serial.print(getDistance(SR04_RIGHT_TRIG, SR04_RIGHT_ECHO));
+  Serial.println(" cm right");
+  Serial.print(getDistance(SR04_LEFT_TRIG, SR04_LEFT_ECHO));
+  Serial.println(" cm left");
+  
 
-  // turnRight();
-  // delay(500);
-    
-  // backward();
-  delay(5000);
-   
+  compassPing();
+  
+  headNorth();
+  
+  delay(2000);
 }
